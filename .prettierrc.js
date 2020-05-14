fbee6c058cb98fcc38c1c6c95e2f7d7b6223fb35
Chirag Jani <chirag@prodsmiths.com>
2020-05-15 03:13:11 +0530

    linting rules eslint prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,10 @@
 module.exports = {
+	proseWrap: 'preserve',
 	semi: true,
 	trailingComma: 'all',
 	singleQuote: true,
 	printWidth: 120,
 	useTabs: true,
 	tabWidth: 2,
+	jsxBracketSameLine: true,
 };
