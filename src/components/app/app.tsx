9db648ed33cde63b474db83300dccbe59dcbf73b
Chirag Jani <chirag@prodsmiths.com>
2020-05-16 04:49:03 +0530

    fixed linting according to improved config, dev server changes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { style } from 'typestyle';
-
-const className = style({ color: 'blue', background: 'red', height: '500px', textAlign: 'center', fontSize: 50 });
+import { ThemeProvider } from 'theme-ui';
+import { theme } from '../../theme';
 
 export type Props = {};
 
-export const App: React.FC<Props> = ({}) => {
-	return <div className={className}>Hello world</div>;
-};
+export const App: React.FC<Props> = () => (
+  <ThemeProvider theme={theme}>
+    <div>Hello world is not</div>
+  </ThemeProvider>
+);
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,29 +3,34 @@ import * as webpack from 'webpack';
 import * as webpackDevServer from 'webpack-dev-server';
 
 const config: webpack.Configuration | webpackDevServer.Configuration = {
-	mode: 'development',
-	devtool: 'source-map',
-	entry: './src/index.tsx',
-	output: {
-		path: path.resolve(__dirname, 'public'),
-		filename: 'bundle.js',
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.jsx'],
-	},
-	module: {
-		rules: [
-			{ test: /\.ts(x?)$/, loader: 'ts-loader' },
-			{
-				enforce: 'pre',
-				test: /\.js(x?)$/,
-				loader: 'source-map-loader',
-			},
-		],
-	},
-	devServer: {
-		contentBase: './public',
-	},
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      { test: /\.ts(x?)$/, loader: 'ts-loader', exclude: /node_modules/ },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    index: 'index.html',
+    contentBase: './public',
+  },
 };
 
 export default config;
