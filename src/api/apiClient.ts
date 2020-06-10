0eb7de24ebfc4d061155e35c52673b5be42128cf
Chirag Jani <chirag@prodsmiths.com>
2020-06-10 13:44:27 +0530

    adding fetch options to url

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,6 +2,7 @@ import { createClient, defaultExchanges } from 'urql';
 import { devtoolsExchange } from '@urql/devtools';
 import { config } from 'configs';
 import { NodeEnvironments } from 'configs/types';
+import { Headers } from './types';
 
 const isDev = config.NODE_ENV === NodeEnvironments.DEVELOPMENT;
 
@@ -15,7 +16,18 @@ const devTools = isDev ? [devtoolsExchange] : [];
  */
 const exchanges = [...devTools, ...defaultExchanges];
 
+// fetch options
+const fetchOptions = (): Headers => {
+  const token: string | null = window.localStorage.getItem('token');
+  if (token && token !== null) {
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : '' },
+    };
+  }
+  return {};
+};
+
 /**
  * client
  */
-export const apiClient = createClient({ url: config.API_URL, exchanges });
+export const apiClient = createClient({ url: config.API_URL, exchanges, fetchOptions });
