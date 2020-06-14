6ef49992080e71d47216cf30e641ecff04230921
Ahn <anhpnnd@gmail.com>
2020-06-14 11:39:32 +0200

    add extra things to help to bypass error

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 // const { pathsToModuleNameMapper } = require('ts-jest/utils');
 // const { compilerOptions } = require('./tsconfig.json');
-
+/** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
@@ -12,7 +12,13 @@ module.exports = {
       tsConfig: './tsconfig.test.json',
     },
   },
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
+  },
   moduleDirectories: ['.', 'node_modules', 'src'],
   // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  moduleNameMapper: {
+    'theme-ui': '<rootDir>/node_modules/theme-ui/dist/index.js',
+  },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
